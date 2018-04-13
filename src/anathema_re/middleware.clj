1316e63ee2180fd22355dca8d84ea03bb1d7b26f
Alex Chythlook <achythlook@gmail.com>
2018-04-13 08:32:20 -0800

    Take another stab at gzipping

--- src/anathema_re/middleware.clj
+++ src/anathema_re/middleware.clj
@@ -1,8 +1,15 @@
 (ns anathema-re.middleware
   (:require [integrant.core :as ig]
-            [ring.middleware.gzip :as rg]))
+            [ring.middleware.gzip :as rg]
+            [ring.middleware.defaults :as defaults]))
 
 (defmethod ig/init-key :anathema-re.middleware/gzip [_ _]
   (fn [a]
     (println "gzipping " a)
     (rg/wrap-gzip a)))
+
+(defmethod ig/init-key :duct.middleware.web/defaults [_ defaults]
+  (fn [a]
+    (-> a
+        rg/wrap-gzip
+        (defaults/wrap-defaults defaults))))
